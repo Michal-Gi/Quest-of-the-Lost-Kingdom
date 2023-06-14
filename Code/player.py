50eee609da2208092cb7334e3123deaba6f3f168
Michał Gierczuk <s24333@pjwstk.edu.pl>
2023-06-14 14:13:45 +0200

    remade movement system and added sprint

--- Code/player.py
+++ Code/player.py
@@ -9,26 +9,35 @@ class Player(pygame.sprite.Sprite):
 
         self.direction = pygame.math.Vector2()
         self.speed = speed
+        self.sprint = 1.0
 
     def input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_w]:
             self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
+        elif keys[pygame.K_s]:
             self.direction.y = 1
         else:
             self.direction.y = 0
 
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_a]:
             self.direction.x = -1
-        elif keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_d]:
             self.direction.x = 1
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]:
+            self.sprint = 3
+        else:
+            self.sprint = 1.0
+
+
+
+
     def move(self, speed):
-        self.rect.center += self.direction * speed
+        self.rect.center += self.direction * speed * self.sprint
 
     def update(self):
         self.input()
