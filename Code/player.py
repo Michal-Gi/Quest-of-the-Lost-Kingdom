9a642875d4d55da86deed2516b8daa4870de0052
Michal-Gi <michalg394@gmail.com>
2023-06-19 10:22:05 +0200

    fixed entire animation system to a point where it actually works as intended

--- Code/player.py
+++ Code/player.py
@@ -7,11 +7,16 @@ from os import path
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, speed, animation_speed, scale):
         super().__init__(groups)
-        self.state_animation_frames = { 'idle' : 2, 'sword' : 6, 'bow' : 13, 'spear' : 8, 'fall' : 6}
+        # animations
+        self.state_animation_frames = { 'idle': 2, 'sword': 6, 'bow': 13, 'spear': 8, 'fall': 6, 'walk': 9}
         self.current_state = 'idle'
-        self.scale = scale
         self.elapsed_time = 0
         self.animation_speed = animation_speed
+        self.in_action = False
+        self.action_cooldown_times = {'idle': 0, 'sword': 200, 'bow': 500, 'spear': 700, 'fall': 200}
+        self.action_time = None
+        # rendering
+        self.scale = scale
         self.back_frame = (0, 0, 64, 64)
         self.left_frame = (0, 64, 64, 64)
         self.front_frame = (0, 128, 64, 64)
@@ -19,6 +24,7 @@ class Player(pygame.sprite.Sprite):
         self.current_frame = self.front_frame
         self.load('../Assets/Characters/Player/idle1.png', scale)
         self.rect = self.image.get_rect(topleft=pos)
+        # movement
         self.direction = pygame.math.Vector2()
         self.speed = speed
         self.sprint = 1.0
@@ -47,16 +53,19 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        # run
         if keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]:
             self.sprint = 3
         else:
             self.sprint = 1.0
 
-        # animations
-        if keys[pygame.K_SPACE]:
+        # attack
+        if keys[pygame.K_SPACE] and not self.in_action:
+            self.elapsed_time = 0
             self.current_state = 'spear'
-        else:
-            self.current_state = 'idle'
+            self.in_action = True
+            self.action_time = pygame.time.get_ticks()
+
 
 
 
@@ -68,17 +77,34 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.input()
+        # character rotation
         if self.direction.x == 1:
             self.current_frame = self.right_frame
         elif self.direction.x == -1:
             self.current_frame = self.left_frame
         elif self.direction.y == -1:
             self.current_frame = self.back_frame
-        else:
+        elif self.direction.y == 1:
             self.current_frame = self.front_frame
+
+        if (self.direction.x != 0 or self.direction.y != 0) and not self.in_action:
+            self.current_state = 'walk'
+        elif self.direction.x == 0 and self.direction.y == 0 and not self.in_action:
+            self.current_state = 'idle'
 
         self.elapsed_time += self.animation_speed
 
         frame = int(self.elapsed_time % self.state_animation_frames.get(self.current_state))
         self.load(f'../Assets/Characters/Player/{self.current_state}{1 + frame}.png', scale=self.scale)
         self.move(self.speed)
+
+        self.cooldowns(self.current_state)
+
+    def cooldowns(self, action):
+        current_time = pygame.time.get_ticks()
+
+        if self.in_action:
+            if current_time - self.action_time >= self.action_cooldown_times.get(action):
+                self.in_action = False
+                self.current_state = 'idle'
+                self.elapsed_time = 0
